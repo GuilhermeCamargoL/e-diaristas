4d3418c41f9ca9dab5d84530a405d3ec7a3753c7
GuilhermeCamargoL <pessoal.guilima@hotmail.com>
2022-10-07 16:00:03 -0300

    Mensagem de feedback para o usuário e ajuste de validação

--- app/Http/Controllers/ServicoController.php
+++ app/Http/Controllers/ServicoController.php
@@ -7,6 +7,11 @@ use App\Models\Servico;
 
 class ServicoController extends Controller
 {
+    /**
+     * Lista todos os Serviços
+     *
+     * @return void
+     */
     public function index()
     {
         $servicos = Servico::paginate(5);
@@ -14,26 +19,49 @@ class ServicoController extends Controller
         return view('servicos.index')->with('servicos', $servicos);
     }
 
+    /**
+     * Formulário de criação do serviço
+     *
+     * @return void
+     */
     public function create()
     {
         return view('servicos.create');
     }
 
-
+    /**
+     * Cria um novo registro no banco de dados
+     *
+     * @param ServicoRequest $request
+     * @return void
+     */
     public function store(ServicoRequest $request)
     {
         $dados = $request->except('_token');
 
         $retornno = Servico::create($dados);
 
-        return redirect()->route('servicos.index');
+        return redirect()->route('servicos.index')->with('mensagem', 'Serviço cadastrado com sucesso!');
     }
 
+    /**
+     * Mostra o formulário preenchido
+     *
+     * @param Servico $servico
+     * @return void
+     */
     public function edit(Servico $servico)
     {
         return view('servicos.edit')->with('servico', $servico);
     }
 
+    /**
+     * Atualiza um registro no banco de dados
+     *
+     * @param ServicoRequest $request
+     * @param [type] $id
+     * @return void
+     */
     public function update(ServicoRequest $request, $id)
     {
         $dados = $request->except(['_token', '_method']);
@@ -42,6 +70,6 @@ class ServicoController extends Controller
 
         $servico->update($dados);
 
-        return redirect()->route('servicos.index');
+        return redirect()->route('servicos.index')->with('mensagem', 'Serviço atualizado com sucesso!');
     }
 }
