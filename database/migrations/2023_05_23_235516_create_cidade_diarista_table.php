6dd88c1cd94a4e00fe854c31ca40cc8f0ac85034
GuilhermeCamargoL <pessoal.guilima@hotmail.com>
2023-05-24 10:12:57 -0300

    Update 2023_05_23_235516_create_cidade_diarista_table.php

--- database/migrations/2023_05_23_235516_create_cidade_diarista_table.php
+++ database/migrations/2023_05_23_235516_create_cidade_diarista_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
 
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users_api');
+            $table->foreign('user_api_id')->references('id')->on('users_api');
 
             $table->unsignedBigInteger('cidade_id');
             $table->foreign('cidade_id')->references('id')->on('cidades');
